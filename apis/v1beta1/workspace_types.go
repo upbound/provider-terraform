a25b40246f55781896d5f7b4adabb03d34a34dc4
Bob Haddleton <bob.haddleton@nokia.com>
2024-02-26 13:21:50 -0600

    Move SYNCED before READY in output
    Signed-off-by: Bob Haddleton <bob.haddleton@nokia.com>

--- apis/v1beta1/workspace_types.go
+++ apis/v1beta1/workspace_types.go
@@ -155,8 +155,8 @@ type WorkspaceStatus struct {
 
 // A Workspace of Terraform Configuration.
 // +kubebuilder:subresource:status
-// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,terraform}
 type Workspace struct {
